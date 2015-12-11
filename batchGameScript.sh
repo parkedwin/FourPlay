0f6149419c6c2bcd17554880e54f1cd13903e944
akma327 <akma327@stanford.edu>
2015-12-10 22:59:30 -0800

    Have 100 no TD game sets

--- batchGameScript.sh
+++ batchGameScript.sh
@@ -1,120 +1,200 @@
-mkdir game21
-python fourplayWebAutomate.py game21 HARD 
-rm -rf game21
+mkdir game51
+python fourplayWebAutomate.py game51 HARD 
+rm -rf game51
 
-mkdir game22
-python fourplayWebAutomate.py game22 HARD 
-rm -rf game22
+mkdir game52
+python fourplayWebAutomate.py game52 HARD 
+rm -rf game52
 
-mkdir game23
-python fourplayWebAutomate.py game23 HARD 
-rm -rf game23
+mkdir game53
+python fourplayWebAutomate.py game53 HARD 
+rm -rf game53
 
-mkdir game24
-python fourplayWebAutomate.py game24 HARD 
-rm -rf game24
+mkdir game54
+python fourplayWebAutomate.py game54 HARD 
+rm -rf game54
 
-mkdir game25
-python fourplayWebAutomate.py game25 HARD 
-rm -rf game25
+mkdir game55
+python fourplayWebAutomate.py game55 HARD 
+rm -rf game55
 
-mkdir game26
-python fourplayWebAutomate.py game26 HARD 
-rm -rf game26
+mkdir game56
+python fourplayWebAutomate.py game56 HARD 
+rm -rf game56
 
-mkdir game27
-python fourplayWebAutomate.py game27 HARD 
-rm -rf game27
+mkdir game57
+python fourplayWebAutomate.py game57 HARD 
+rm -rf game57
 
-mkdir game28
-python fourplayWebAutomate.py game28 HARD 
-rm -rf game28
+mkdir game58
+python fourplayWebAutomate.py game58 HARD 
+rm -rf game58
 
-mkdir game29
-python fourplayWebAutomate.py game29 HARD 
-rm -rf game29
+mkdir game59
+python fourplayWebAutomate.py game59 HARD 
+rm -rf game59
 
-mkdir game30
-python fourplayWebAutomate.py game30 HARD 
-rm -rf game30
+mkdir game60
+python fourplayWebAutomate.py game60 HARD 
+rm -rf game60
 
-mkdir game31
-python fourplayWebAutomate.py game31 HARD 
-rm -rf game31
+mkdir game61
+python fourplayWebAutomate.py game61 HARD 
+rm -rf game61
 
-mkdir game32
-python fourplayWebAutomate.py game32 HARD 
-rm -rf game32
+mkdir game62
+python fourplayWebAutomate.py game62 HARD 
+rm -rf game62
 
-mkdir game33
-python fourplayWebAutomate.py game33 HARD 
-rm -rf game33
+mkdir game63
+python fourplayWebAutomate.py game63 HARD 
+rm -rf game63
 
-mkdir game34
-python fourplayWebAutomate.py game34 HARD 
-rm -rf game34
+mkdir game64
+python fourplayWebAutomate.py game64 HARD 
+rm -rf game64
 
-mkdir game35
-python fourplayWebAutomate.py game35 HARD 
-rm -rf game35
+mkdir game65
+python fourplayWebAutomate.py game65 HARD 
+rm -rf game65
 
-mkdir game36
-python fourplayWebAutomate.py game36 HARD 
-rm -rf game36
+mkdir game66
+python fourplayWebAutomate.py game66 HARD 
+rm -rf game66
 
-mkdir game37
-python fourplayWebAutomate.py game37 HARD 
-rm -rf game37
+mkdir game67
+python fourplayWebAutomate.py game67 HARD 
+rm -rf game67
 
-mkdir game38
-python fourplayWebAutomate.py game38 HARD 
-rm -rf game38
+mkdir game68
+python fourplayWebAutomate.py game68 HARD 
+rm -rf game68
 
-mkdir game39
-python fourplayWebAutomate.py game39 HARD 
-rm -rf game39
+mkdir game69
+python fourplayWebAutomate.py game69 HARD 
+rm -rf game69
 
-mkdir game40
-python fourplayWebAutomate.py game40 HARD 
-rm -rf game40
+mkdir game70
+python fourplayWebAutomate.py game70 HARD 
+rm -rf game70
 
-mkdir game41
-python fourplayWebAutomate.py game41 HARD 
-rm -rf game41
+mkdir game71
+python fourplayWebAutomate.py game71 HARD 
+rm -rf game71
 
-mkdir game42
-python fourplayWebAutomate.py game42 HARD 
-rm -rf game42
+mkdir game72
+python fourplayWebAutomate.py game72 HARD 
+rm -rf game72
 
-mkdir game43
-python fourplayWebAutomate.py game43 HARD 
-rm -rf game43
+mkdir game73
+python fourplayWebAutomate.py game73 HARD 
+rm -rf game73
 
-mkdir game44
-python fourplayWebAutomate.py game44 HARD 
-rm -rf game44
+mkdir game74
+python fourplayWebAutomate.py game74 HARD 
+rm -rf game74
 
-mkdir game45
-python fourplayWebAutomate.py game45 HARD 
-rm -rf game45
+mkdir game75
+python fourplayWebAutomate.py game75 HARD 
+rm -rf game75
 
-mkdir game46
-python fourplayWebAutomate.py game46 HARD 
-rm -rf game46
+mkdir game76
+python fourplayWebAutomate.py game76 HARD 
+rm -rf game76
 
-mkdir game47
-python fourplayWebAutomate.py game47 HARD 
-rm -rf game47
+mkdir game77
+python fourplayWebAutomate.py game77 HARD 
+rm -rf game77
 
-mkdir game48
-python fourplayWebAutomate.py game48 HARD 
-rm -rf game48
+mkdir game78
+python fourplayWebAutomate.py game78 HARD 
+rm -rf game78
 
-mkdir game49
-python fourplayWebAutomate.py game49 HARD 
-rm -rf game49
+mkdir game79
+python fourplayWebAutomate.py game79 HARD 
+rm -rf game79
 
-mkdir game50
-python fourplayWebAutomate.py game50 HARD 
-rm -rf game50
+mkdir game80
+python fourplayWebAutomate.py game80 HARD 
+rm -rf game80
+
+mkdir game81
+python fourplayWebAutomate.py game81 HARD 
+rm -rf game81
+
+mkdir game82
+python fourplayWebAutomate.py game82 HARD 
+rm -rf game82
+
+mkdir game83
+python fourplayWebAutomate.py game83 HARD 
+rm -rf game83
+
+mkdir game84
+python fourplayWebAutomate.py game84 HARD 
+rm -rf game84
+
+mkdir game85
+python fourplayWebAutomate.py game85 HARD 
+rm -rf game85
+
+mkdir game86
+python fourplayWebAutomate.py game86 HARD 
+rm -rf game86
+
+mkdir game87
+python fourplayWebAutomate.py game87 HARD 
+rm -rf game87
+
+mkdir game88
+python fourplayWebAutomate.py game88 HARD 
+rm -rf game88
+
+mkdir game89
+python fourplayWebAutomate.py game89 HARD 
+rm -rf game89
+
+mkdir game90
+python fourplayWebAutomate.py game90 HARD 
+rm -rf game90
+
+mkdir game91
+python fourplayWebAutomate.py game91 HARD 
+rm -rf game91
+
+mkdir game92
+python fourplayWebAutomate.py game92 HARD 
+rm -rf game92
+
+mkdir game93
+python fourplayWebAutomate.py game93 HARD 
+rm -rf game93
+
+mkdir game94
+python fourplayWebAutomate.py game94 HARD 
+rm -rf game94
+
+mkdir game95
+python fourplayWebAutomate.py game95 HARD 
+rm -rf game95
+
+mkdir game96
+python fourplayWebAutomate.py game96 HARD 
+rm -rf game96
+
+mkdir game97
+python fourplayWebAutomate.py game97 HARD 
+rm -rf game97
+
+mkdir game98
+python fourplayWebAutomate.py game98 HARD 
+rm -rf game98
+
+mkdir game99
+python fourplayWebAutomate.py game99 HARD 
+rm -rf game99
+
+mkdir game100
+python fourplayWebAutomate.py game100 HARD 
+rm -rf game100
 
